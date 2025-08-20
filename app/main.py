d93eba19d72fd68864ef5af3fe966bdda0d55979
Seshcloud <seshadri.careers@gmail.com>
2025-08-19 20:37:55 -0400

    Initial commit - FastAPI website with CI/CD

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="FastAPI Website CI/CD Example")
+
+templates = Jinja2Templates(directory="templates")
+
+# serve static files (css/js)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Home Page"})
